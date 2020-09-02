e4d3b9ca616336ccf37f8fa7c6da6fd07ec0ccf1
Sean Birmingham <seannbirmingham98@gmail.com>
2020-09-02 15:30:11 -0400

    finished 09_dictionaries.py - 11_args.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,11 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
+def is_even(num):
+    if num % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -8,5 +13,4 @@ num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
-
+print(is_even(num))
