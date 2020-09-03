8e1031c1ce7e19e5335c963c764547a3f0297444
Sean Birmingham <seannbirmingham98@gmail.com>
2020-09-02 22:09:28 -0400

    finished 12_scopes.py and 13_file_io.py

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,8 +4,12 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
+
 def change_x():
+    # This keyword allows you to modify a variable outside of the current scope
+    global x
     x = 99
+
 
 change_x()
 
@@ -16,9 +20,11 @@ print(x)
 # This nested function has a similar problem.
 
 def outer():
-    y = 120
+    y = [120]
 
     def inner():
+        # This keyword is used to work with variables inside nested funtions
+        nonlocal y
         y = 999
 
     inner()
